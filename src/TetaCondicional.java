dcc89793a150d0389b4922d34d8540c4b667158f
Jeff <jefff>
2019-08-26 21:38:45 -0300

    a

--- src/TetaCondicional.java
+++ src/TetaCondicional.java
@@ -1,12 +1,13 @@
 
 public class TetaCondicional {
-	public static void main(string[]args) {
+	public static void main(String[] args) {
 		System.out.println("testando Condicionais");
-	    int idade = 20;
+	    int idade = 10;
 	    boolean naoTenhoDinheiro = true;
 	    int quantidadedePessoas = 3;
 	    
-	    if(idade>18 && pessoas>=3) { System.out.println("pode entrar!");
+	    if(idade>18 || quantidadedePessoas>=3) 
+	    { System.out.println("pode entrar!");
 	    	
 	    }
 	    else { System.out.println("Nao pode entrar!");
